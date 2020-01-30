4e521bf157dd42158951153a638fbc63e21a2358
Matthew Smalley <dev@divisiblebyzero.co.uk>
2020-01-30 18:58:20 +0000

    Initial revision

---
+++ src/main/java/uk/co/divisiblebyzero/restandgraph/entities/Country.java
@@ -0,0 +1,22 @@
+package uk.co.divisiblebyzero.restandgraph.entities;
+
+public class Country {
+    private String name;
+    private String language;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
